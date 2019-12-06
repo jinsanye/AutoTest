33e0f936c8a47411f6b31e11bfedadbebdc44d52
dengzq <dengzq@http://git.wadecn.com:18082>
2019-12-06 19:49:22 +0500

    添加注释，删除无效依赖

--- Chapter1/src/main/java/com/asiainfo/testng/DependTest.java
+++ Chapter1/src/main/java/com/asiainfo/testng/DependTest.java
@@ -7,6 +7,8 @@ public class DependTest {
     @Test
     public void testA(){
         System.out.println("DependTest.testA");
+        //testA将会抛出异常，testB不会被执行
+        throw new RuntimeException();
     }
 
     @Test(dependsOnMethods = {"testA"})
