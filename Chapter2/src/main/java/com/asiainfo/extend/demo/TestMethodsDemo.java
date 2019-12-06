bae63d4d17603fec8107a38bb03120c7b72590f1
dengzq <dengzq@http://git.wadecn.com:18082>
2019-12-07 02:13:29 +0500

    生成extendReports测试报告

---
+++ Chapter2/src/main/java/com/asiainfo/extend/demo/TestMethodsDemo.java
@@ -0,0 +1,30 @@
+package com.asiainfo.extend.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestMethodsDemo {
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,2);
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa","aaa");
+    }
+
+    @Test
+    public void logDemo(){
+        Reporter.log("This log writtend by myself");
+        throw new RuntimeException("This RuntimeException belong to me");
+    }
+
+}
